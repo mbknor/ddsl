53c13b8262c6eb9bb1c781bfd9b4cdb9df3b83be
Morten Kjetland <mbk@kjetland.com>
2011-01-16 16:32:56 +0100

    moved main-test of zdao into zdao-file

--- src/main/scala/com/kjetland/ddsl/Dao.scala
+++ src/main/scala/com/kjetland/ddsl/Dao.scala
@@ -202,3 +202,35 @@ class ZDao (val hosts : String) extends Dao with Watcher {
 
 
 
+object ZDaoTestMain{
+
+  def main ( args : Array[String]){
+    val log = Logger.getLogger( getClass )
+    try{
+      doStuff
+    }catch{
+      case x:Exception => log.error("error", x)
+    }
+
+
+  }
+
+  def doStuff {
+    println("hw")
+
+    val hosts = "localhost:2181"
+    val dao = new ZDao( hosts )
+
+    val sid = ServiceId("test", "http", "testService", "1.0")
+    val sl = ServiceLocation(sid, "http://localhost/url", "http://localhost/test", 10.0, new DateTime())
+    Thread.sleep( 100 )
+    val sl2 = ServiceLocation(sid, "http://localhost:90/url", "http://localhost:90/test", 9.0, new DateTime())
+
+    dao.update( sl )
+    dao.update( sl2 )
+
+    dao.getSLs( sid).foreach{println( _ )}
+
+    Thread.sleep(100000)
+  }
+}
--- src/main/scala/com/kjetland/ddsl/Main.scala
+++ src/main/scala/com/kjetland/ddsl/Main.scala
@@ -12,35 +12,3 @@ import org.apache.log4j.Logger
  * To change this template use File | Settings | File Templates.
  */
 
-object Main{
-
-  def main ( args : Array[String]){
-    val log = Logger.getLogger( getClass )
-    try{
-      doStuff
-    }catch{
-      case x:Exception => log.error("error", x)
-    }
-
-
-  }
-
-  def doStuff {
-    println("hw")
-
-    val hosts = "localhost:2181"
-    val dao = new ZDao( hosts )
-
-    val sid = ServiceId("test", "http", "testService", "1.0")
-    val sl = ServiceLocation(sid, "http://localhost/url", "http://localhost/test", 10.0, new DateTime())
-    Thread.sleep( 100 )
-    val sl2 = ServiceLocation(sid, "http://localhost:90/url", "http://localhost:90/test", 9.0, new DateTime())
-
-    dao.update( sl )
-    dao.update( sl2 )
-
-    dao.getSLs( sid).foreach{println( _ )}
-
-    Thread.sleep(100000)
-  }
-}
