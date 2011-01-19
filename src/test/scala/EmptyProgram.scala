bd26bd1a418ee6dad8ca41c735b95ede607f2e28
Morten Kjetland <mbk@kjetland.com>
2011-01-19 21:07:39 +0100

    empty sbt idea compile hack

---
+++ src/test/scala/EmptyProgram.scala
@@ -0,0 +1,14 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mortenkjetland
+ * Date: 1/19/11
+ * Time: 11:05 AM
+ * small "program" that can be executed with sbt compile in idea to force sbt compile in idea.
+ */
+
+object EmptyProgram{
+  def main(args : Array[String]){
+    println("done")
+  }
+}
