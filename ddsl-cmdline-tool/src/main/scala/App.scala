c9721f512270f81d26931f6b970612ac3a566e4a
Morten Kjetland <mbk@kjetland.com>
2012-09-10 15:13:59 +0200

    Proper parsing stuff

--- ddsl-cmdline-tool/src/main/scala/App.scala
+++ ddsl-cmdline-tool/src/main/scala/App.scala
@@ -7,8 +7,13 @@ case class DdslError(val msg: String ) extends Exception
 
 case class CmdBase()
 
-case class CmdParsed(val cmd:String, val data:String) extends CmdBase
+case class CmdParsed(val cmd:String, val data:Option[String])
 
+case class Result(val success : Boolean, val msg : String)
+
+case class OkResult(override val msg : String) extends Result(true, msg)
+case class ErrorResult(override val msg : String) extends Result(false, msg)
+case class ExitResult() extends OkResult("Quiting")
 object App {
 
   def main(args: Array[String]) {
@@ -16,15 +21,23 @@ object App {
     while ( true ) {
       val cmdString = readLine()
       val cmd = parseCmd(cmdString)
-      cmd match {
-        case Some(x: CmdParsed) => {
-          sendResponse(true, "echo: " + x.cmd + " " + x.data)
-        }
-        case _ => {
-          sendResponse(false, "error parsing command")
-        }
+      val r : Result = cmd.cmd match {
+        case "exit" => ExitResult()
+        case "help" => processHelp()
+        case _ => ErrorResult("Unknown cmd: " + cmd.cmd)
       }
+
+      sendResponse(r.success, r.msg)
+      if ( r.isInstanceOf[ExitResult]) {
+        // Quiting
+        return
+      }
+
     }
+  }
+
+  def processHelp() : Result = {
+    return OkResult("See documentation..")
   }
 
   def sendResponse(success:Boolean, msg : String) {
@@ -32,12 +45,12 @@ object App {
     println( status + msg )
   }
 
-  def parseCmd(cmdString : String) : Option[CmdBase] = {
+  def parseCmd(cmdString : String) : CmdParsed = {
     val i = cmdString.indexOf(' ')
     if ( i <= 0) {
-      return None
+      return CmdParsed(cmdString, None)
     }
-    return Some(CmdParsed(cmdString.substring(0,i), cmdString.substring(i+1)))
+    return CmdParsed(cmdString.substring(0,i), Some(cmdString.substring(i+1)))
   }
 
 }
